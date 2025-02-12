07d47a41a5ed1b9088766c523a18e0d5b72c4c96
Arun <arunofficial400@gmail.com>
2025-02-12 17:10:13 +0530

    created

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component, ElementRef, ViewChild, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgClass, NgFor } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgClass, NgFor],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+  todoList: TodoItem[] = [];
+  newTask: string = "";
+  @ViewChild('todoText') todoInputRef: ElementRef<HTMLInputElement> | null = null;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedTodoList = localStorage.getItem('todoList');
+      if (storedTodoList) {
+        this.todoList = JSON.parse(storedTodoList);
+      }
+    }
+  }
+
+  addTask(text: string): void {
+    if (text.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: text.trim(),
+        completed: false
+      };
+      this.todoList.push(newTodoItem);
+      if (this.todoInputRef) {
+        this.todoInputRef.nativeElement.value = '';
+      }
+      this.saveTodoList();
+    }
+  }
+
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    this.saveTodoList();
+  }
+
+  toggleCompleted(id: number): void {
+    const todoItem = this.todoList.find(item => item.id === id);
+    if (todoItem) {
+      todoItem.completed = !todoItem.completed;
+      this.saveTodoList();
+    }
+  }
+
+  saveTodoList(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('todoList', JSON.stringify(this.todoList));
+    }
+  }
+}
